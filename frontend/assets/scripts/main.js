a907ea3e1d92063c581026fb7d02062eab902a22
alibkm95 <ali.bkm95@gmail.com>
2024-02-19 19:41:16 +0330

    latest add section and box html templates created and there styles applied. swiper logics added

--- frontend/assets/scripts/main.js
+++ frontend/assets/scripts/main.js
@@ -41,3 +41,31 @@ window.addEventListener('load', () => {
   })
 
 })
+
+const swiper = new Swiper('.swiper', {
+
+  speed: 400,
+  spaceBetween: 20,
+  loop: true,
+
+  breakpoints: {
+    576: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    992: {
+      slidesPerView: 3
+    }
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    dynamicBullets: true,
+  },
+  grabCursor: true,
+  autoplay: {
+    delay: 5000
+  }
+})
