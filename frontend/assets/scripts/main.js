daa2a26020b616282f35d624a4c7d0cd2629e8b9
alibkm95 <ali.bkm95@gmail.com>
2024-02-20 12:53:19 +0330

    fix: elements sliding in swiper fixed

--- frontend/assets/scripts/main.js
+++ frontend/assets/scripts/main.js
@@ -42,7 +42,35 @@ window.addEventListener('load', () => {
 
 })
 
-const swiper = new Swiper('.swiper', {
+const latestSwiper = new Swiper('.latestSwiper', {
+
+  speed: 400,
+  spaceBetween: 20,
+  loop: true,
+
+  breakpoints: {
+    576: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    992: {
+      slidesPerView: 3
+    }
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    dynamicBullets: true,
+  },
+  grabCursor: true,
+  autoplay: {
+    delay: 5000
+  }
+})
+
+const topViewedSwiper = new Swiper('.topViewedSwiper', {
 
   speed: 400,
   spaceBetween: 20,
