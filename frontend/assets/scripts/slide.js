0330f084c7f04130e0d87e4c5b6c9344bc3eb23d
alibkm95 <ali.bkm95@gmail.com>
2024-02-22 19:21:38 +0330

    similar ads section html templates created and its styles applied

--- frontend/assets/scripts/slide.js
+++ frontend/assets/scripts/slide.js
@@ -54,6 +54,34 @@ const topViewedSwiper = new Swiper('.topViewedSwiper', {
   }
 })
 
+const similarSwiper = new Swiper('.similarSwiper', {
+
+  speed: 400,
+  spaceBetween: 20,
+  loop: true,
+
+  breakpoints: {
+    576: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    992: {
+      slidesPerView: 3
+    }
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    dynamicBullets: true,
+  },
+  grabCursor: true,
+  autoplay: {
+    delay: 5000
+  }
+})
+
 const scrollerElem = document.querySelector('.trust__body')
 
 if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches && scrollerElem !== null) {
