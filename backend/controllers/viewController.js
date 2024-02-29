6da4bd2164e501f9e6883480b7f9efd461b45fe0
alibkm95 <ali.bkm95@gmail.com>
2024-02-29 21:01:31 +0330

    feat(get all views data): views controllers getAllViews coded

--- backend/controllers/viewController.js
+++ backend/controllers/viewController.js
@@ -3,11 +3,26 @@ const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
 
 const getAllViews = async (req, res) => {
-  res.json({ msg: 'getAllViews' })
+
+  const views = await Views.find()
+
+  const viewsGroup = groupData(views)
+
+  res.status(StatusCodes.OK).json({ viewsGroup })
 }
 
 const getSingleAdViews = async (req, res) => {
   res.json({ msg: 'getSingleAdViews' })
+}
+
+const groupData = (data) => {
+  const result = data.reduce((acc, item) => {
+    const date = new Date(item.createdAt).toISOString().slice(0, 10);
+    acc[date] = (acc[date] || 0) + 1;
+    return acc;
+  }, {});
+
+  return Object.entries(result);
 }
 
 module.exports = { getAllViews, getSingleAdViews }
