2e2d3f3427c5c67463d45c6c0a93f596c7eeed43
alibkm95 <ali.bkm95@gmail.com>
2024-03-01 16:46:11 +0330

    feat(create new request): request controllers createRequest function coded

--- backend/controllers/requestController.js
+++ backend/controllers/requestController.js
@@ -2,11 +2,34 @@ const Request = require('../models/Request')
 const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
 
-const { StatusCodes } = require('http-status-codes')
-const CustomError = require('../errors')
-
 const createRequest = async (req, res) => {
-  res.json({ msg: 'createRequest' })
+
+  const { userId } = req.user
+  const { selectedAd } = req.body
+
+  if (!selectedAd) {
+    throw new CustomError.BadRequestError('for request, candidate ad must be provided')
+  }
+
+  const alreadyRequested = await Request.findOne({ user: userId, ad: selectedAd })
+
+  if (alreadyRequested && alreadyRequested.status === 'pending') {
+    throw new CustomError.BadRequestError('error! duplicated request')
+  }
+
+  const requestObject = {
+    user: userId,
+    ad: selectedAd,
+    status: 'pending'
+  }
+
+  const newRequest = await Request.create(requestObject)
+
+  if (!newRequest) {
+    throw new CustomError.BadRequestError('error! creating new request failed')
+  }
+
+  res.status(StatusCodes.CREATED).json({ msg: 'request created successfully' })
 }
 
 const getAllRequests = async (req, res) => {
