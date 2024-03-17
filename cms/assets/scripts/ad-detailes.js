30cfb85e676e7f9aa7ec1738ac74bc2a12e9a2dd
alibkm95 <ali.bkm95@gmail.com>
2024-03-17 20:45:11 +0330

    ad form and tooltips html templates created and their styles applied

--- cms/assets/scripts/ad-detailes.js
+++ cms/assets/scripts/ad-detailes.js
@@ -1,3 +1,4 @@
+
 const viewChartWrapper = document.getElementById('charts-view-wrapper')
 const requestChartWrapper = document.getElementById('charts-request-wrapper')
 
@@ -65,4 +66,143 @@ const requestChart = new Chart(viewChartWrapper, {
       }
     }
   }
+})
+
+const primaryPriceHelp = tippy('#primary-price-help', {
+  content: `
+    <p>
+      + default value is (0) and considered as negotiable
+    </p>
+    <p>
+      + if ad type is mortgage and rent this value considered as mortgage price otherwise it's represents the property's total price
+    </p>
+  `,
+  placement: 'top',
+  arrow: true,
+  animation: 'fade',
+  theme: 'light-border',
+  trigger: 'click',
+  allowHTML: true,
+})
+
+const secondaryPriceHelp = tippy('#secondary-price-help', {
+  content: `
+    <p>
+      + default value is (0) and considered as negotiable
+    </p>
+    <p>
+      + if ad type is mortgage and rent this value considered as rent price otherwise it's represents the property's price per square meter
+    </p>
+  `,
+  placement: 'top',
+  arrow: true,
+  animation: 'fade',
+  theme: 'light-border',
+  trigger: 'click',
+  allowHTML: true,
+})
+
+const coverImageHelp = tippy('#cover-image-help', {
+  content: `
+    <p>
+      + this field is required
+    </p>
+    <p>
+      + maximum size of the image file must be less than 10MB
+    </p>
+    <p>
+      + as soon as the cover image uploaded, this field automatically fills with uploaded images direct link
+    </p>
+  `,
+  placement: 'top',
+  zIndex: 100000,
+  arrow: true,
+  animation: 'fade',
+  theme: 'light-border',
+  trigger: 'click',
+  allowHTML: true,
+})
+
+const panoramaImageHelp = tippy('#panorama-image-help', {
+  content: `
+    <p>
+      + this field is required
+    </p>
+    <p>
+      + maximum size of the image file must be less than 10MB
+    </p>
+    <p>
+      + as soon as the panorama image uploaded, this field automatically fills with uploaded images direct link
+    </p>
+  `,
+  placement: 'top',
+  arrow: true,
+  animation: 'fade',
+  theme: 'light-border',
+  trigger: 'click',
+  allowHTML: true,
+})
+
+const roomsHelp = tippy('#rooms-help', {
+  content: `
+    <p>
+      + this field is required
+    </p>
+    <p>
+      + for properties that do not have rooms, consider the value (0)
+    </p>
+  `,
+  placement: 'top',
+  arrow: true,
+  animation: 'fade',
+  theme: 'light-border',
+  trigger: 'click',
+  allowHTML: true,
+})
+
+const yocHelp = tippy('#YOC-help', {
+  content: `
+    <p>
+      + (0) is default and considered as unknown
+    </p>
+  `,
+  placement: 'top',
+  zIndex: 100000,
+  arrow: true,
+  animation: 'fade',
+  theme: 'light-border',
+  trigger: 'click',
+  allowHTML: true,
+})
+
+const locationHelp = tippy('#location-help', {
+  content: `
+    <p>
+      + if you do not send a value to this field, it will be treated as unknown
+    </p>
+  `,
+  placement: 'top',
+  arrow: true,
+  animation: 'fade',
+  theme: 'light-border',
+  trigger: 'click',
+  allowHTML: true,
+})
+
+const floorHelp = tippy('#floor-help', {
+  content: `
+    <p>
+      + default value for both floor level and total floors fields is (0)
+    </p>
+    <p>
+      + The value 0 will be considered as the ground floor
+    </p>
+  `,
+  placement: 'top',
+  zIndex: 100000,
+  arrow: true,
+  animation: 'fade',
+  theme: 'light-border',
+  trigger: 'click',
+  allowHTML: true,
 })
